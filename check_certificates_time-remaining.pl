87656dc055a44db7198df3b113c0f21716eba28e
Gunnar Haslinger <gh.github@hitco.at>
2021-02-04 21:13:33 +0100

    Support HEX-encoded SAN-Names

--- check_certificates_time-remaining.pl
+++ check_certificates_time-remaining.pl
@@ -6,19 +6,21 @@ use Getopt::Long qw(GetOptions);
 use Data::Dumper;
 
 my $verbose; my $help; my $debug;
-my $limit = 30;
+my $limit = 20;
 my $LetsEncrytpCertDir="/etc/letsencrypt/live";
 my $ApacheConfigs="/etc/apache2/sites-enabled/*";
 my %certlist;
+my $renew = 0;
 
-GetOptions('limit=i' => \$limit, 'v' => \$verbose, 'd|vv' => \$debug, 'help|?|h' => \$help);
+GetOptions('limit=i' => \$limit, 'v' => \$verbose, 'd|vv' => \$debug, 'help|?|h' => \$help, 'renew' => \$renew);
 
 if ($help) {
 print "Commandline arguments:\n";
-print "Without Arguments: Only shows Certificates valid for less than 30 days.\n";
+print "Without Arguments: Only shows Certificates valid for less than 25 days.\n";
 print "-limit=20 ... show only certificates valid for less than n (20) days\n";
 print "-v ... Verbose Output\n";
 print "-vv ... Debug Output\n";
+print "-renew ... erneuere automatisch\n";
 exit(1);
 }
 
@@ -54,13 +56,29 @@ sub checkcert($)
   my $days_left = $date_valid->delta_days(DateTime->now())->delta_days();
   my $name = $cert->subject(); $name = $1 if $name =~ /CN=(\S+)/i;
   if ($days_left <= $limit || $verbose) {
-     print "$name" . " "x(25-length($name)) . " "x(4-length($days_left)) . $days_left . " Days difference now to " . $date_valid->strftime("%d.%m.%Y %H:%M:%S %Z") . "\n";
-     print " "x30 . "File: $certfile\n";
+     print "$name" . " "x(30-length($name)) . " "x(4-length($days_left)) . $days_left . " Days difference now to " . $date_valid->strftime("%d.%m.%Y %H:%M:%S %Z") . "\n";
+     print " "x35 . "File: $certfile\n";
      my $SAN = $cert->extensions_by_name()->{'subjectAltName'}->value();
+     #print "### $SAN\n";
+     if ($SAN =~ /^\#/) { # if SAN is encoded as HEX-String
+         $SAN =~ s/^\#//; # cut of the starting "#" and decode HEX to ASCII-String
+         $SAN =~ s/(..)/chr(hex($1))/eg;
+     }
      $SAN =~ s/[^\w\d\.-]/./g;
      $SAN =~ s/^\dU?\.+//;
-     $SAN =~ s/\.\./\n                                       /g;
-     print " "x30 . "AltName: " . $SAN . "\n";
+     $SAN =~ s/^..\.\.//; # am Beginn zwei Zeichen + 2 Punkte - dann entfernen
+     $SAN =~ s/^.\.\.//; # am Beginn ein Zeichen + 2 Punkte - dann entfernen
+     $SAN =~ s/\.\./\n                                            /g;
+     print " "x35 . "AltName: " . $SAN . "\n";
+     if ($renew) {
+        print "=== START automatische Erneuerung $name ===\n";
+        my $renewscript="./anforderung_$name.sh";
+        if (! -f $renewscript) { print "   Error: Script fehlt: $renewscript\n";
+           } else
+           { system($renewscript);
+           }
+        print "=== ENDE  automatische Erneuerung $name ===\n";
+     }
   }
 }
 
